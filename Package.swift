8be38c87c3940a41687a56faf4ffaca6adc6aee2
Keen <zhekeen90@gmail.com>
2025-05-28 20:09:57 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,15 @@
 // swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
     name: "CarbonKit",
-    platforms: [
-        .iOS(.v11) // Pastikan ini adalah target minimum iOS yang benar untuk proyek Anda
-    ],
     products: [
-        .library(
-            name: "CarbonKit",
-            targets: ["CarbonKit"]
-        ),
+        .library(name: "CarbonKit", targets: ["CarbonKit"]),
     ],
     targets: [
-        .target(
-            name: "CarbonKit",
-            path: "CarbonKit", // Pastikan ini mengarah ke folder CarbonKit yang benar
-            sources: [
-                "CarbonKit/CarbonSwipeRefresh.m",
-                "CarbonKit/CarbonTabSwipeNavigation.m",
-                "CarbonKit/CarbonTabSwipeScrollView.m",
-                "CarbonKit/CarbonTabSwipeSegmentedControl.m"
-            ],
-            publicHeadersPath: "CarbonKit" // Menyatakan bahwa file header umum berada dalam folder CarbonKit
-        )
+        .target(name: "CarbonKit", 
+                path: "./CarbonKit",
+        .testTarget(name: "CarbonKitTests", dependencies: ["CarbonKit"], path: "./CarbonKitTests"),
     ]
 )
