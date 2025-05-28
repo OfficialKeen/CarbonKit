f3377df2740c715e942fce60f6b52aa8b3a3f55f
Keen <zhekeen90@gmail.com>
2025-05-28 15:05:33 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -12,24 +12,12 @@ let package = Package(
             targets: ["CarbonKit"] // This should match the name of your target
         ),
     ],
-    dependencies: [
-        // Add any external dependencies here
-        // .package(url: "https://github.com/some/dependency.git", from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "CarbonKit",
-            path: "CarbonKit", // Ensure this path is correct relative to the location of your Package.swift file
-            sources: ["CarbonKit.h", "CarbonSwipeRefresh.h", "CarbonSwipeRefresh.m",
-                      "CarbonTabSwipeNavigation.h", "CarbonTabSwipeNavigation.m",
-                      "CarbonTabSwipeScrollView.h", "CarbonTabSwipeScrollView.m",
-                      "CarbonTabSwipeSegmentedControl.h", "CarbonTabSwipeSegmentedControl.m"],
-            publicHeadersPath: "CarbonKit" // If your public headers are in the root of the CarbonKit folder
-        ),
-        .testTarget(
-            name: "CarbonKitTests",
-            dependencies: ["CarbonKit"],
-            path: "CarbonKitTests" // Ensure this path is correct relative to the location of your Package.swift file
+            dependencies: [],
+            path: "CarbonKit",
+            publicHeadersPath: "CarbonKit"
         )
     ]
 )
