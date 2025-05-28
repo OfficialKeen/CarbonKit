a5aca9044834876be2d61404568c141260122ace
Keen <zhekeen90@gmail.com>
2025-05-28 20:29:51 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,29 @@
 // swift-tools-version:5.3
-
 import PackageDescription
 
 let package = Package(
     name: "CarbonKit",
+    platforms: [
+        .iOS(.v11) // Specify the minimum iOS version your package supports
+    ],
     products: [
-        .library(name: "CarbonKit", targets: ["CarbonKit"]),
+        .library(
+            name: "CarbonKit",
+            targets: ["CarbonKit"]
+        ),
+    ],
+    dependencies: [
+        // Add any external dependencies here
     ],
     targets: [
-        .target(name: "CarbonKit", 
-                path: "./CarbonKit",
-        .testTarget(name: "CarbonKitTests", dependencies: ["CarbonKit"], path: "./CarbonKitTests"),
+        .target(
+            name: "CarbonKit",
+            dependencies: [], // List any external dependencies your package has
+            path: "CarbonKit",
+            sources: [
+                "CarbonKit.h"
+            ],
+            publicHeadersPath: "."
+        )
     ]
 )
