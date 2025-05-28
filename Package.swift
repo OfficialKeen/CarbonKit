4945d07edbcbe1f0569fd60dd4446556e70d0980
Keen <zhekeen90@gmail.com>
2025-05-28 19:58:09 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,25 @@ import PackageDescription
 let package = Package(
     name: "CarbonKit",
     platforms: [
-        .iOS(.v11) // Ensure this is the correct minimum deployment target for your library
+        .iOS(.v11) // Pastikan ini adalah target minimum iOS yang benar untuk proyek Anda
     ],
     products: [
         .library(
             name: "CarbonKit",
-            targets: ["CarbonKit"] // This should match the name of your target
+            targets: ["CarbonKit"]
         ),
     ],
     targets: [
         .target(
             name: "CarbonKit",
-            dependencies: [],
-            path: "CarbonKit",
-            publicHeadersPath: "CarbonKit"
+            path: "CarbonKit", // Pastikan ini mengarah ke folder CarbonKit yang benar
+            sources: [
+                "CarbonKit/CarbonSwipeRefresh.m",
+                "CarbonKit/CarbonTabSwipeNavigation.m",
+                "CarbonKit/CarbonTabSwipeScrollView.m",
+                "CarbonKit/CarbonTabSwipeSegmentedControl.m"
+            ],
+            publicHeadersPath: "CarbonKit" // Menyatakan bahwa file header umum berada dalam folder CarbonKit
         )
     ]
 )
