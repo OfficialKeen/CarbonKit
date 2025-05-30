48cc49eaca14ed5899afefc7adb451f8012e22dc
Keen <zhekeen90@gmail.com>
2025-05-30 22:00:31 +0700

    Package

--- Package.swift
+++ Package.swift
@@ -4,28 +4,21 @@ import PackageDescription
 let package = Package(
     name: "CarbonKit",
     platforms: [
-        .iOS(.v12), // Adjust the platform and version as needed
+        .iOS(.v11) // Specify the minimum iOS version your package supports
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CarbonKit",
-            targets: ["CarbonKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["CarbonKit"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CarbonKit",
-            dependencies: [], // List any Swift or Objective-C dependencies here
-            path: "Sources/CarbonKit",
+            path: "CarbonKit",
             sources: [
                 "CarbonKit.h"
-            ],
+            ]
         )
     ]
 )
